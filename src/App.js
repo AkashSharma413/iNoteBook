84229979c5e3c7f2e7e24487898e24d0db4f765f
Akash Sharma <aakash.sharma413@gmail.com>
2022-10-10 23:15:40 +0530

    Created note context using context API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
+import "./App.css";
+import About from "./components/About";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NoteState from "./context/notes/NoteState";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is iNotebook</h1>
-    </div>
+    <NoteState>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </NoteState>
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,21 @@
+import React, { useContext, useEffect } from "react";
+import noteContext from "../context/notes/noteContext";
+
+const About = () => {
+  const a = useContext(noteContext);
+
+  useEffect(() => {
+    a.update();
+    // eslint-disable-next-line 
+  }, []);
+
+  return (
+    <>
+      <h3>
+        This is {a.state.name} and he is in class {a.state.class}
+      </h3>
+    </>
+  );
+};
+
+export default About;
